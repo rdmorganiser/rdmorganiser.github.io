e4516691a1802cb18d6795f242447450d156f170
Janine <janinevier@gmx.de>
2017-11-29 17:28:15 +0100

    first changes according color of markers

--- js/map.js
+++ js/map.js
@@ -11,9 +11,18 @@ $(document).ready(function() {
     var source   = document.getElementById('marker-template').innerHTML;
     var template = Handlebars.compile(source);
 
+    var greenIcon = new L.Icon({
+       iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+       shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+       iconSize: [25, 41],
+       iconAnchor: [12, 41],
+       popupAnchor: [1, -34],
+       shadowSize: [41, 41]
+    });
+
     $.each(_locations, function (key, location) {
         var html = template(location);
-        var marker = L.marker([location.lon, location.lat]).addTo(map);
+        var marker = L.marker([location.lon, location.lat], {icon: greenIcon}).addTo(map);
         marker.bindPopup(html);
     });
 });
